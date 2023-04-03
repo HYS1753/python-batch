938e2a970d2b5f512446aa748c19a2d6ca1b48d7
HYS1753 <hys1753@naver.com>
2023-04-04 00:49:37 +0900

    [batch][road_address_etl][추가] 도로명 주소 파일 Copy 형식으로 변환 기능 추가

--- src/etl/road_address_etl.py
+++ src/etl/road_address_etl.py
@@ -1,1 +1,78 @@
 # TODO
+import os
+import sys
+import time
+from datetime import datetime
+
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(os.path.dirname(__file__))), 'lib'))
+from LogManager import LogManager
+
+def road_name_address_etl(logger, f_path, f_name, field_sep, line_sep):
+    result = False
+    logger.trace_log("info", f"{f_name} road_name_address etl Start")
+    try:
+        s_time = time.time()
+        # rnaddr file format
+        # 도로명주소관리번호|법정동코드|시도명|시군구명|읍면동명|리명|산여부|번지|호|도로명코드|도로명|지하여부|건물본번|건물부번|
+        # 행정동코드|행정동명|기초구역번호(우편번호)|이전도로명주소|효력발생일|공동주택구분|이동사유코드|건축물대장건물명|시군구용건물명|비고
+        with open(os.path.join(f_path, f"{f_name}.copy"), 'w') as wf:
+            with open(os.path.join(f_path, f_name), 'r') as rf:
+                for line in rf.readlines():
+                    #wf.write(line.replace('|', field_sep) + line_sep)
+                    now_time = datetime.now().strftime("%y-%m-%d %H:%M:%S")
+                    line_list = line.split('|')
+                    extn_line_list = list()
+                    extn_line_list.append(line_list[0])     # rna_mgmt_num | 도로명주소관리번호
+                    extn_line_list.append(line_list[1])     # legal_dong_code | 법정동코드
+                    extn_line_list.append(line_list[2])     # sido | 시도명
+                    extn_line_list.append(line_list[3])     # sigungu | 시군구명
+                    extn_line_list.append(line_list[4])     # eupmyeondong | 읍면동명
+                    extn_line_list.append(line_list[5])     # li | 리명
+                    extn_line_list.append(line_list[6])     # san_ysno | 산여부
+                    extn_line_list.append(line_list[7])     # land_num | 번지
+                    extn_line_list.append(line_list[8])     # ho | 호
+                    extn_line_list.append(line_list[9])     # rna_code | 도로명코드
+                    extn_line_list.append(line_list[10])    # road_name | 도로명
+                    extn_line_list.append(line_list[11])    # basement_ysno | 지하여부
+                    extn_line_list.append(line_list[12])    # bldg_main_num | 건물본번
+                    extn_line_list.append(line_list[13])    # bldg_sub_num | 건물부번
+                    extn_line_list.append(line_list[14])    # admin_dong_code | 행정동코드
+                    extn_line_list.append(line_list[15])    # admin_dong_name | 행정동명
+                    extn_line_list.append(line_list[16])    # zip_code | 기초구역번호(우편번호)
+                    extn_line_list.append(line_list[17])    # prev_rna | 이전도로명주소
+                    extn_line_list.append(line_list[18])    # effect_date | 효력발생일
+                    extn_line_list.append(line_list[19])    # apt_ysno | 공동주택구분
+                    extn_line_list.append(line_list[21])    # bldg_name | 건축물대장건물명
+                    extn_line_list.append("python-batch")   # cret_id | 생성자ID
+                    extn_line_list.append(now_time)         # cret_dttm | 생성일시
+                    extn_line_list.append("python-batch")   # amnd_id | 수정자ID
+                    extn_line_list.append(now_time)         # amnd_dttm | 수정일시
+                    wf.write(field_sep.join(extn_line_list) + line_sep)
+        result = True
+        logger.trace_log("info", f"{f_name} road_name_address etl Success. {time.time()-s_time:.4f} sec")
+    except Exception as e:
+        logger.trace_log("error", f"Exception: {e}")
+    return result
+
+def main():
+    # Custom logger setting
+    logger = LogManager(FILENAME='road_address_etl', LEVEL='INFO')
+
+    # variable setting
+    work_dir = os.path.join(os.path.dirname(os.path.abspath(os.path.dirname(__file__))), 'data', 'road_address')
+    work_file_list = os.listdir(work_dir)
+    logger.trace_log("debug", f"road_address_etl work directory set {work_dir}")
+    field_sep = '\t'
+    line_sep = '\n'
+
+    # road name address Start
+    rnaddr_file_list = [x for x in work_file_list if x.startswith("rnaddrkor") and x.endswith(".txt")]
+    for f_name in rnaddr_file_list:
+        rnaddr_result = road_name_address_etl(logger, work_dir, f_name, field_sep, line_sep)
+        if not rnaddr_result:
+            logger.trace_log("error", f"etl error occurred in {f_name} file")
+            break
+
+
+if __name__ == "__main__":
+    main()
